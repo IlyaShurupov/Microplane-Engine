1f79379b535bd8ef85d8a608bc3cc147ee8001ff
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-07-29 20:52:50 +0200

    Update Graphics.cpp

--- ME2D/Graphics.cpp
+++ ME2D/Graphics.cpp
@@ -1,4 +1,4 @@
-﻿//============================================================================
+//============================================================================
 // Microplane Engine - ME2D
 //----------------------------------------------------------------------------
 // Copyright (c) 2018 Ivan Kmeťo
@@ -30,10 +30,12 @@ Graphics::Graphics()
 	factory = NULL;
 	renderTarget = NULL;
 	brush = NULL;
+	fillBrush = NULL;
 }
 
 Graphics::~Graphics()
 {
+	if (fillBrush) fillBrush->Release();
 	if (brush) brush->Release();
 	if (renderTarget) renderTarget->Release();
 	if (factory) factory->Release();
@@ -53,6 +55,9 @@ bool Graphics::Init(HWND windowHandle)
 	res = renderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &brush);
 	if (res != S_OK) return false;
 
+	res = renderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &fillBrush);
+	if (res != S_OK) return false;
+
 	return true;
 }
 
@@ -62,14 +67,68 @@ void Graphics::ClearScreen(float r, float g, float b)
 	renderTarget->Clear(D2D1::ColorF(r, g, b));
 }
 
-void Graphics::DrawCircle(float x, float y, float radius, float r, float g, float b, float a)
-{
+
+//-----------------------------------------------[OBJECTS: 2D GEOMETRY]-----------------------------------------------
+
+void Graphics::RenderCircleRGBA(float x, float y, float radius, float r, float g, float b, float a, float strokesize) {
 	brush->SetColor(D2D1::ColorF(r, g, b, a));
-	renderTarget->DrawEllipse(D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius), brush, 3.0f);
+
+	D2D1_ELLIPSE circle = D2D1::Ellipse(
+		D2D1::Point2F(x, y),
+		radius,
+		radius
+	);
+	renderTarget->DrawEllipse(circle, brush, strokesize);
 }
 
-void Graphics::DrawRectangle(float x, float y, float rectleft, float recttop, float rectright, float rectbottom, float r, float g, float b, float a)
-{
+void Graphics::RenderCircleRGBAF(float x, float y, float radius, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
 	brush->SetColor(D2D1::ColorF(r, g, b, a));
-	renderTarget->DrawRectangle(D2D1::RectF(rectleft, recttop, rectright, rectbottom), brush, 3.0f);
+	fillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+
+	D2D1_ELLIPSE circle = D2D1::Ellipse(
+		D2D1::Point2F(x, y),
+		radius,
+		radius
+	);
+	renderTarget->DrawEllipse(circle, brush, strokesize);
+	renderTarget->FillEllipse(circle, fillBrush);
+}
+
+
+void Graphics::RenderEllipseRGBA(float x, float y, float xRadius, float yRadius, float r, float g, float b, float a, float strokesize) {
+	brush->SetColor(D2D1::ColorF(r, g, b, a));
+
+	D2D1_ELLIPSE ellipse = D2D1::Ellipse(
+		D2D1::Point2F(x, y),
+		xRadius,
+		yRadius
+	);
+	renderTarget->DrawEllipse(ellipse, brush, strokesize);
+}
+
+
+void Graphics::RenderSquareRGBA(float x, float y, float size, float r, float g, float b, float a, float strokesize) {
+	brush->SetColor(D2D1::ColorF(r, g, b, a));
+
+	D2D1_RECT_F square = D2D1::RectF(
+		x - (size / 2),
+		y - (size / 2),
+		x + (size / 2),
+		y + (size / 2)
+	);
+	renderTarget->DrawRectangle(square, brush, strokesize);
+}
+
+void Graphics::RenderSquareRGBAF(float x, float y, float size, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
+	brush->SetColor(D2D1::ColorF(r, g, b, a));
+	fillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+
+	D2D1_RECT_F square = D2D1::RectF(
+		x - (size / 2),
+		y - (size / 2),
+		x + (size / 2),
+		y + (size / 2)
+	);
+	renderTarget->DrawRectangle(square, brush, strokesize);
+	renderTarget->FillRectangle(square, fillBrush);
 }
