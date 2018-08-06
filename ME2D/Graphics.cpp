1531a0ecf3baaabcb941d4ac813bcbee4f309757
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2018-08-06 21:45:37 +0200

    Update Graphics.cpp

--- ME2D/Graphics.cpp
+++ ME2D/Graphics.cpp
@@ -23,40 +23,61 @@
 
 #include "stdafx.h"
 #include "Graphics.h"
+#include "Logger.h"
 
+
+void ASSERT_D3D(HRESULT value, std::string description, std::string msgboxTitle) {
+	MessageBox(nullptr, description.c_str(), msgboxTitle.c_str(), MB_OK | MB_ICONERROR);
+	Log_Open("debug.log");
+	Error(value);
+	Log_Write(description);
+	Log_Close();
+}
 
 Graphics::Graphics()
 {
-	factory = NULL;
-	renderTarget = NULL;
-	brush = NULL;
-	fillBrush = NULL;
+	m_pFactory = NULL;
+	m_pRenderTarget = NULL;
+	m_pMainBrush = NULL;
+	m_pFillBrush = NULL;
 }
 
 Graphics::~Graphics()
 {
-	if (fillBrush) fillBrush->Release();
-	if (brush) brush->Release();
-	if (renderTarget) renderTarget->Release();
-	if (factory) factory->Release();
+	if (m_pFillBrush) m_pFillBrush->Release();
+	if (m_pMainBrush) m_pMainBrush->Release();
+	if (m_pRenderTarget) m_pRenderTarget->Release();
+	if (m_pFactory) m_pFactory->Release();
 }
 
 bool Graphics::Init(HWND windowHandle)
 {
-	HRESULT res = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &factory);
-	if (res != S_OK) return false;
+	HRESULT errchck001 = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pFactory);
+	if (errchck001 != S_OK) {
+		ASSERT_D3D(errchck001, "[ERRD2D1001] D2D1 Factory failed to initialize!", "Error");
+		return false;
+	}
 
 	RECT rect;
 	GetClientRect(windowHandle, &rect);
 
-	res = factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(windowHandle, D2D1::SizeU(rect.right, rect.bottom)), &renderTarget);
-	if (res != S_OK) return false;
+	HRESULT errchck002 = m_pFactory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(), D2D1::HwndRenderTargetProperties(windowHandle, D2D1::SizeU(rect.right, rect.bottom)), &m_pRenderTarget);
+	if (errchck002 != S_OK) {
+		ASSERT_D3D(errchck002, "[ERRD2D1002] D2D1 Factory failed to create ID2D1HwndRenderTarget!", "Error");
+		return false;
+	}
 
-	res = renderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &brush);
-	if (res != S_OK) return false;
+	HRESULT errchck003 = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &m_pMainBrush);
+	if (errchck003 != S_OK) {
+		ASSERT_D3D(errchck003, "[ERRD2D1003] Failed to create Solid Color Brush!", "Error");
+		return false;
+	}
 
-	res = renderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &fillBrush);
-	if (res != S_OK) return false;
+	HRESULT errchck004 = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(0, 0, 0, 0), &m_pFillBrush);
+	if (errchck004 != S_OK) {
+		ASSERT_D3D(errchck004, "[ERRD2D1004] Failed to create Solid Color Brush!", "Error");
+		return false;
+	}
 
 	return true;
 }
@@ -64,51 +85,77 @@ bool Graphics::Init(HWND windowHandle)
 
 void Graphics::ClearScreen(float r, float g, float b)
 {
-	renderTarget->Clear(D2D1::ColorF(r, g, b));
+	m_pRenderTarget->Clear(D2D1::ColorF(r, g, b));
 }
 
 
 //-----------------------------------------------[OBJECTS: 2D GEOMETRY]-----------------------------------------------
 
 void Graphics::RenderCircleRGBA(float x, float y, float radius, float r, float g, float b, float a, float strokesize) {
-	brush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	D2D1_ELLIPSE circle = D2D1::Ellipse(
 		D2D1::Point2F(x, y),
 		radius,
 		radius
 	);
-	renderTarget->DrawEllipse(circle, brush, strokesize);
+	m_pRenderTarget->DrawEllipse(circle, m_pMainBrush, strokesize);
 }
 
-void Graphics::RenderCircleRGBAF(float x, float y, float radius, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
-	brush->SetColor(D2D1::ColorF(r, g, b, a));
-	fillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+void Graphics::RenderCircleRGBAF(float x, float y, float radius, float r, float g, float b, float a, float strokesize) {
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pFillBrush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	D2D1_ELLIPSE circle = D2D1::Ellipse(
 		D2D1::Point2F(x, y),
 		radius,
 		radius
 	);
-	renderTarget->DrawEllipse(circle, brush, strokesize);
-	renderTarget->FillEllipse(circle, fillBrush);
+	m_pRenderTarget->DrawEllipse(circle, m_pMainBrush, strokesize);
+	m_pRenderTarget->FillEllipse(circle, m_pFillBrush);
+}
+
+void Graphics::RenderCircleRGBASF(float x, float y, float radius, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pFillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+
+	D2D1_ELLIPSE circle = D2D1::Ellipse(
+		D2D1::Point2F(x, y),
+		radius,
+		radius
+	);
+	m_pRenderTarget->DrawEllipse(circle, m_pMainBrush, strokesize);
+	m_pRenderTarget->FillEllipse(circle, m_pFillBrush);
 }
 
 
 void Graphics::RenderEllipseRGBA(float x, float y, float xRadius, float yRadius, float r, float g, float b, float a, float strokesize) {
-	brush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	D2D1_ELLIPSE ellipse = D2D1::Ellipse(
 		D2D1::Point2F(x, y),
 		xRadius,
 		yRadius
 	);
-	renderTarget->DrawEllipse(ellipse, brush, strokesize);
+	m_pRenderTarget->DrawEllipse(ellipse, m_pMainBrush, strokesize);
+}
+
+void Graphics::RenderEllipseRGBAF(float x, float y, float xRadius, float yRadius, float r, float g, float b, float a, float strokesize) {
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pFillBrush->SetColor(D2D1::ColorF(r, g, b, a));
+
+	D2D1_ELLIPSE ellipse = D2D1::Ellipse(
+		D2D1::Point2F(x, y),
+		xRadius,
+		yRadius
+	);
+	m_pRenderTarget->DrawEllipse(ellipse, m_pMainBrush, strokesize);
+	m_pRenderTarget->FillEllipse(ellipse, m_pFillBrush);
 }
 
 
 void Graphics::RenderSquareRGBA(float x, float y, float size, float r, float g, float b, float a, float strokesize) {
-	brush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	D2D1_RECT_F square = D2D1::RectF(
 		x - (size / 2),
@@ -116,12 +163,12 @@ void Graphics::RenderSquareRGBA(float x, float y, float size, float r, float g, 
 		x + (size / 2),
 		y + (size / 2)
 	);
-	renderTarget->DrawRectangle(square, brush, strokesize);
+	m_pRenderTarget->DrawRectangle(square, m_pMainBrush, strokesize);
 }
 
-void Graphics::RenderSquareRGBAF(float x, float y, float size, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
-	brush->SetColor(D2D1::ColorF(r, g, b, a));
-	fillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+void Graphics::RenderSquareRGBAF(float x, float y, float size, float r, float g, float b, float a, float strokesize) {
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pFillBrush->SetColor(D2D1::ColorF(r, g, b, a));
 
 	D2D1_RECT_F square = D2D1::RectF(
 		x - (size / 2),
@@ -129,6 +176,20 @@ void Graphics::RenderSquareRGBAF(float x, float y, float size, float r, float g,
 		x + (size / 2),
 		y + (size / 2)
 	);
-	renderTarget->DrawRectangle(square, brush, strokesize);
-	renderTarget->FillRectangle(square, fillBrush);
+	m_pRenderTarget->DrawRectangle(square, m_pMainBrush, strokesize);
+	m_pRenderTarget->FillRectangle(square, m_pFillBrush);
+}
+
+void Graphics::RenderSquareRGBASF(float x, float y, float size, float r, float g, float b, float a, float strokesize, float fill_r, float fill_g, float fill_b, float fill_a) {
+	m_pMainBrush->SetColor(D2D1::ColorF(r, g, b, a));
+	m_pFillBrush->SetColor(D2D1::ColorF(fill_r, fill_g, fill_b, fill_a));
+
+	D2D1_RECT_F square = D2D1::RectF(
+		x - (size / 2),
+		y - (size / 2),
+		x + (size / 2),
+		y + (size / 2)
+	);
+	m_pRenderTarget->DrawRectangle(square, m_pMainBrush, strokesize);
+	m_pRenderTarget->FillRectangle(square, m_pFillBrush);
 }
