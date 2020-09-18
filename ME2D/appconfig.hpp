c94124e7ce1931d36aebbcb3186750520d07f5e9
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-09-18 20:04:30 +0200

    Update appconfig.hpp

--- ME2D/appconfig.hpp
+++ ME2D/appconfig.hpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Microplane Engine - ME2D
 //----------------------------------------------------------------------------
-// Copyright (c) 2018 Ivan Kmeťo
+// Copyright (c) 2018, 2020 Ivan Kmeťo
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
