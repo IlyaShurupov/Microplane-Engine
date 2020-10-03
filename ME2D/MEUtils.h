25bbe4d95f796ab090e211fe5172ec8b0433a6ca
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-10-03 13:28:08 +0200

    Update MEUtils.h

--- ME2D/MEUtils.h
+++ ME2D/MEUtils.h
@@ -1,7 +1,7 @@
 //============================================================================
 // Microplane Engine - ME2D
 //----------------------------------------------------------------------------
-// Copyright (c) 2018 Ivan Kmeťo
+// Copyright (c) 2018, 2020 Ivan Kmeťo
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
@@ -23,8 +23,8 @@
 
 #pragma once
 
-#include "stdafx.h"
-#include "Logger.h"
+#include "stdafx.hpp"
+#include "appconfig.hpp"
 
 
 static uint16_t getMousePosX; //Capture Mouse Position X from lParam in LRESULT CALLBACK WindowProc [main.cpp]
@@ -34,13 +34,16 @@ static uint16_t getMousePosY; //Capture Mouse Position Y from lParam in LRESULT 
 static const std::string s_debugMsg = std::string(MEDEF_ENG_ENGABBV) + " " + std::string(MEDEF_ENG_ENGVERD) + " (" + std::string(MEDEF_ENG_ENGVERS) + ") (Debug)";
 static std::wstring MEUTL_DBG_ENGDMSG(s_debugMsg.begin(), s_debugMsg.end());
 
+static const std::string s_logEngineInfo = MEDEF_USR_APPNAME " | " MEDEF_USR_APPDEVN " | " MEDEF_USR_APPVERD " | " MEDEF_USR_APPVERS "\n"
+                                           MEDEF_ENG_ENGNAME " (" MEDEF_ENG_ENGABBV ") | " MEDEF_ENG_ENGVERD " | " MEDEF_ENG_ENGVERS "\n--------------\n";
+
 
 static void ASSERT_ME2D(HRESULT value, std::string description, std::string msgboxTitle) {
 	MessageBox(nullptr, description.c_str(), msgboxTitle.c_str(), MB_OK | MB_ICONERROR);
-	Log_Open("debug.log");
-	Error(value);
-	Log_Write(description + "\n");
-	Log_Close();
+	logutil::file_open("debug.log");
+	__error(value);
+	logutil::write(description + "\n");
+	logutil::file_close();
 }
 
 static void ASSERT_ME2D_AUTO(HRESULT value, std::string description, std::string msgboxTitle) {
