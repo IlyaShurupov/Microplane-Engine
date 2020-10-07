536dc4fc39ffde8b4afa4b95629fecfa9de73796
Ivan Kmeťo <32756443+kkmzero@users.noreply.github.com>
2020-10-07 16:23:17 +0200

    fix compiler warnings

--- ME2D/SpriteSheet.h
+++ ME2D/SpriteSheet.h
@@ -1,7 +1,7 @@
-﻿//============================================================================
+//============================================================================
 // Microplane Engine - ME2D
 //----------------------------------------------------------------------------
-// Copyright (c) 2018 Ivan Kmeťo
+// Copyright (c) 2018, 2020 Ivan Kmeťo
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
@@ -32,7 +32,8 @@ class SpriteSheet
 	Graphics* gfx;
 	ID2D1Bitmap* bmp;								// Bitmap loaded file and converted to Direct2D format
 
-	int spriteWidth, spriteHeight;
+	int spriteWidth;
+	int spriteHeight;
 	int spritesAccross;
 
 public:
